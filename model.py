7e89190f1202b5977c82a208585e935fb6cb12c9
malyvsen <iceflamecode@gmail.com>
2018-08-16 15:29:53 +0200

    simplified model now learns properly

--- model.py
+++ model.py
@@ -5,15 +5,12 @@ import data
 inputs = tf.placeholder(tf.float32, shape=(None, data.num_attributes))
 
 class classifier:
-    layers = []
-    layers.append(tf.layers.dense(inputs=inputs, units=4, activation=tf.nn.relu))
-    layers.append(tf.layers.dense(inputs=inputs, units=8, activation=tf.nn.relu))
-    layers.append(tf.layers.dense(inputs=inputs, units=4, activation=tf.nn.relu))
-    layers.append(tf.layers.dense(inputs=inputs, units=data.num_classes, activation=tf.nn.softmax))
+    layers = [inputs]
+    layers.append(tf.layers.dense(inputs=layers[-1], units=data.num_classes, activation=tf.nn.softmax))
 
 # optimization-related
 same_class_probabilities = tf.matmul(classifier.layers[-1], tf.transpose(classifier.layers[-1]))
 log_same_class = tf.log(same_class_probabilities)
 sameness = tf.placeholder(tf.float32, shape=(None, None))
-loss = -tf.reduce_mean(tf.multiply(log_same_class, sameness))
-optimizer = tf.train.AdamOptimizer(.01).minimize(loss)
+loss = -tf.reduce_sum(tf.multiply(log_same_class, sameness))
+optimizer = tf.train.AdamOptimizer(1e-3).minimize(loss)

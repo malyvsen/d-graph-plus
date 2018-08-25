c77bee1cb677c0bedf84c8b8aa9c5867d11a260a
malyvsen <iceflamecode@gmail.com>
2018-08-25 17:34:06 +0200

    better comments on model

--- model.py
+++ model.py
@@ -8,9 +8,10 @@ class classifier:
     layers = [inputs]
     layers.append(tf.layers.dense(inputs=layers[-1], units=data.num_classes, activation=tf.nn.softmax))
 
-# optimization-related
+# maximize/minimize the probability that i and j are in the same class
+# depending on what the expert says and with what certainty
 same_class_probabilities = tf.matmul(classifier.layers[-1], tf.transpose(classifier.layers[-1]))
 log_same_class = tf.log(same_class_probabilities)
-sameness = tf.placeholder(tf.float32, shape=(None, None))
+sameness = tf.placeholder(tf.float32, shape=(None, None)) # values of w from the original paper
 loss = -tf.reduce_sum(tf.multiply(log_same_class, sameness))
 optimizer = tf.train.AdamOptimizer(1e-2).minimize(loss)

08140d3e80553e095c796fdc6a405c7e364f4c46
malyvsen <iceflamecode@gmail.com>
2018-08-29 22:44:54 +0200

    weight function, examples more similar to original paper

--- data.py
+++ data.py
@@ -10,6 +10,7 @@ num_cannot = 12
 
 
 examples, classes = make_moons(num_examples)
+examples += np.random.normal(scale=5e-2, size=np.shape(examples))
 
 must_link = []
 while len(must_link) < num_must:
@@ -31,8 +32,24 @@ while len(cannot_link) < num_cannot:
 
 
 def rbf(a, b):
-    '''a measure of similarity between examples a and b'''
-    return np.exp(-np.sum(np.square(a - b)))
+    '''
+    radial basis function: https://www.wikiwand.com/en/Radial_basis_function
+    a measure of similarity between examples a and b
+    '''
+    gamma = 1
+    return np.exp(-gamma * np.sum(np.square(a - b)))
+
+
+def weight(a, b):
+    '''
+    see equation (5) in original paper
+    a measure of similarity between examples a and b
+    '''
+    epsilon = 0.5
+    rbf_val = rbf(a, b)
+    if rbf_val > epsilon:
+        return 2 * rbf_val - 1
+    return 2 / num_classes - 1
 
 
 def sameness(a, b):
@@ -43,8 +60,8 @@ def sameness(a, b):
     if (a, b) in must_link or (b, a) in must_link:
         return 1
     if (a, b) in cannot_link or (b, a) in cannot_link:
-        return -2
-    return (rbf(a, b) - 1) * 1e-1
+        return -1
+    return weight(examples[a], examples[b]) * 1e-2
 
 
 def batch(size=num_examples):

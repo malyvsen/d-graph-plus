2a1ed317ff6f7eeb328c02205094f0bb7363ac3c
malyvsen <iceflamecode@gmail.com>
2018-11-02 15:22:21 +0100

    add wine dataset (no working model yet)

---
+++ d_graph_plus/tasks/wine.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.datasets import load_wine
+
+
+# dataset
+num_attributes = 13
+num_classes = 3
+
+data = load_wine()
+examples = data.data
+classes = data.target
+
+
+# hyperparameters
+num_must = 0
+num_cannot = 0
+neighborhood_size = 4
+auxiliary_weight = 1
+regularizer = tf.contrib.layers.l2_regularizer(0.0)
+batch_size = len(examples) # examples
+min_batch_must = 0 # pairs of examples
+min_batch_cannot = 0 # pairs of examples
+learning_rate = 1e-3
+num_episodes = 64
+
+
+class model:
+    input = tf.placeholder(tf.float32, shape=(None, num_attributes))
+    layers = [input]
+    output = tf.layers.dense(inputs=layers[-1], units=num_classes, activation=tf.nn.softmax, kernel_regularizer=regularizer)
+    layers.append(output)
